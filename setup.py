45cf57a14b41b4f9d7543985237443a552d598f8
Maurits van Rees <maurits@vanrees.org>
2016-08-08 17:59:06 +0200

    Reindented setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,38 +2,39 @@ from setuptools import setup, find_packages
 
 version = '2.0.2.dev0'
 
-setup(name='Products.validation',
-      version=version,
-      description="Data validation package for Archetypes",
-      long_description=open("README.txt").read() + "\n" + \
-                       open("CHANGES.txt").read(),
-      classifiers=[
+setup(
+    name='Products.validation',
+    version=version,
+    description="Data validation package for Archetypes",
+    long_description=(open("README.txt").read() + "\n" +
+                      open("CHANGES.txt").read()),
+    classifiers=[
         "Framework :: Zope2",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
-        ],
-      keywords='Zope catalog index',
-      author='Benjamin Saller',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='https://pypi.python.org/pypi/Products.validation',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['Products'],
-      include_package_data=True,
-      zip_safe=False,
-      extras_require=dict(
+    ],
+    keywords='Zope catalog index',
+    author='Benjamin Saller',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://pypi.python.org/pypi/Products.validation',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['Products'],
+    include_package_data=True,
+    zip_safe=False,
+    extras_require=dict(
         test=[
             'Products.Archetypes[test]',
         ]
-      ),
-      install_requires=[
-          'setuptools',
-          'zope.i18n',
-          'zope.i18nmessageid',
-          'zope.interface',
-          'Acquisition',
-          'DateTime',
-          'Zope2',
-      ],
-      )
+    ),
+    install_requires=[
+        'setuptools',
+        'zope.i18n',
+        'zope.i18nmessageid',
+        'zope.interface',
+        'Acquisition',
+        'DateTime',
+        'Zope2',
+    ],
+)
