4c1760f8546170f4eecc47201dbb96c7ac48be1c
Maurits van Rees <maurits@vanrees.org>
2016-08-08 17:59:42 +0200

    Added classifier for Python 2.6.
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
         "Framework :: Zope2",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
     ],
     keywords='Zope catalog index',
