0e4110a8792e17393ce5963d4667fb87841f7628
Maurits van Rees <maurits@vanrees.org>
2024-08-12 16:24:25 +0200

    Cleanup new test_doctests.py.

--- Products/validation/tests/test_doctests.py
+++ Products/validation/tests/test_doctests.py
@@ -1,14 +1,11 @@
+from unittest import TestSuite
+
 import doctest
-import unittest
 
 
 def test_suite():
-    from unittest import TestSuite
-
     suite = TestSuite()
-
-    doctests = ("Products.validation.validators.ExpressionValidator",)
-    for module in doctests:
-        suite.addTest(doctest.DocTestSuite(module))
-
+    suite.addTest(
+        doctest.DocTestSuite("Products.validation.validators.ExpressionValidator")
+    )
     return suite
