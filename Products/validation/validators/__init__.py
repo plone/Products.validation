1f59b677cf284ad89cb9e590ded8f186b1ad2b57
Maurits van Rees <maurits@vanrees.org>
2024-08-12 12:16:05 +0200

    Explain the imports in Products/validation/validators/__init__.py
    Change the order, isort was complaining about most of the imports not being at the top.

--- Products/validation/validators/__init__.py
+++ Products/validation/validators/__init__.py
@@ -1,29 +1,32 @@
-from Products.validation.validators.ExpressionValidator import ExpressionValidator
-from Products.validation.validators.RangeValidator import RangeValidator
-from Products.validation.validators.RegexValidator import RegexValidator
+# This module gathers a list of validators, and registers them to the service.
+#
+# Three of these are backwards compatibility imports, marked with 'noqa F401'
+# for flake8.  Of these, the RangeValidator and RegexValidator are included in
+# baseValidators, so that is fine.
+# ExpressionValidator is not included.  This is because you need to create an
+# instance of a validator class and register this.  The ExpressionValidator
+# class is fine as base for your custom validators, but no one thought of
+# a default instance to add here.
+from Products.validation.validators.BaseValidators import baseValidators
+from Products.validation.validators.EmptyValidator import (
+    validatorList as emptyValidators,
+)
+from Products.validation.validators.ExpressionValidator import (  # noqa F401
+    ExpressionValidator,
+)
+from Products.validation.validators.IdValidator import validatorList as idValidators
+from Products.validation.validators.RangeValidator import RangeValidator  # noqa F401
+from Products.validation.validators.RegexValidator import RegexValidator  # noqa F401
+from Products.validation.validators.SupplValidators import (
+    validatorList as supplValidators,
+)
 
 
 validators = []
-
-from Products.validation.validators.BaseValidators import baseValidators
-
-
 validators.extend(baseValidators)
-
-from Products.validation.validators.EmptyValidator import validatorList
-
-
-validators.extend(validatorList)
-
-from Products.validation.validators.SupplValidators import validatorList
-
-
-validators.extend(validatorList)
-
-from Products.validation.validators.IdValidator import validatorList
-
-
-validators.extend(validatorList)
+validators.extend(emptyValidators)
+validators.extend(supplValidators)
+validators.extend(idValidators)
 
 
 def initialize(service):
