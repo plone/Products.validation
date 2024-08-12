68ca1dbb01f8a65c69fd5001d77048cce957d261
Maurits van Rees <maurits@vanrees.org>
2024-08-12 12:42:27 +0200

    Drop support for Plone 5.2 and for Python 3.7 and lower.
    Only Plone 6.0 and 6.1 are supported now.
    Note that in Plone 6.1, this package is no longer tested together with the core.
    It has been moved to the ecosystem versions.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "2.1.4.dev0"
+version = "3.0.0.dev0"
 
 setup(
     name="Products.validation",
@@ -11,31 +11,31 @@ setup(
     long_description=(open("README.rst").read() + "\n" + open("CHANGES.rst").read()),
     classifiers=[
         "Development Status :: 6 - Mature",
-        "Framework :: Zope2",
-        "Framework :: Zope :: 4",
+        "Framework :: Zope",
         "Framework :: Zope :: 5",
         "Operating System :: OS Independent",
         "Framework :: Plone",
-        "Framework :: Plone :: 5.1",
-        "Framework :: Plone :: 5.2",
         "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: 6.1",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     keywords="Zope validation regex email",
     author="Benjamin Saller",
     author_email="plone-developers@lists.sourceforge.net",
-    url="https://pypi.org/project/Products.validation",
+    url="https://github.com/plone/Products.validation",
     license="GPL",
     packages=find_packages(),
     namespace_packages=["Products"],
     include_package_data=True,
     zip_safe=False,
     extras_require={},
+    python_requires=">=3.8",
     install_requires=[
         "setuptools",
         "zope.i18n",
